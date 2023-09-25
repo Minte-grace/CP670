3654d454093ef3c9e3855ed4934ef196d5ee3d53
Minte-grace <mintesnottsegaye23@gmail.com>
2023-09-25 19:40:25 -0400

    Added 3 activities for part 2

--- app/src/main/java/com/example/androidassignments/LoginActivity.java
+++ app/src/main/java/com/example/androidassignments/LoginActivity.java
@@ -1,24 +1,25 @@
 package com.example.androidassignments;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
-        Button homeButton = findViewById(R.id.home_button);
+        Button homeButton = findViewById(R.id.login_button);
 
         homeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                Intent intent = new Intent(LoginActivity.this, ListItemsActivity.class);
                 startActivity(intent);
             }
         });
